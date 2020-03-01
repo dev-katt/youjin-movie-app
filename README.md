66421309a4ac3e677d5e830bbb8d45bdef753306
youjin lee <youjinlee1997@gmail.com>
2020-03-01 20:19:25 +0900

    200301 공부끗!

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
 노마드 코더의 'React Fundamentals 2019' 수강중
 
-✔ 2020-02-29
+**✔ 2020-02-29**
+
+- #0 0 Introduction
+- #0 1 Requirements
+- #0 2 Theory Requirements
+- #0 3 Why React
+- #1 0 Creating your first React App
+- #1 1 Creating a Github Repository
+
+**✔ 2020-03-01**
+
+- #1 2 How does React work?
+- #2 0 Creating your first React Component
+- #2 1 Reusable Components with JSX + Props
--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ hate, fav }) {
+  return (
+    <h1>
+      I like {fav}. {hate}
+    </h1>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      <Food fav="Potato" hate="no" />
+      <Food fav="Pasta" hate="yes" />
+      <Food fav="Goguma" hate="no" />
     </div>
   );
 }
